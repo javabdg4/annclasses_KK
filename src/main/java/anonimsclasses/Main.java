2a31ac226aff016a7c395f489227efa9dbf20ad8
Katarzyna <kkubikka@gmail.com>
2018-09-07 19:03:12 +0200

    changes

--- src/main/java/anonimsclasses/Main.java
+++ src/main/java/anonimsclasses/Main.java
@@ -1,26 +1,55 @@
 package anonimsclasses;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.Button;
+
 public class Main {
 
-   public interface Button{
-       void click();
-       void mouseOn();
+    public interface ButtonT {
+        void click();
+
+//        void mouseOn();
     }
 
-    public static void action(){
-        Button button=new Button() {
+    public static void action() {
+        ButtonT buttonT = new ButtonT() {
+
             public void click() {
                 System.out.println("przycisk klikniety");
             }
 
-            public void mouseOn() {
-                System.out.println("mysza poruszona");
+//            public void mouseOn() {
+//                System.out.println("mysza poruszona");
+//            }
+        };
+        buttonT.click();
+
+    }
+
+    public static void main(String[] args) {
+        ButtonT buttonT = () -> System.out.println("Przycisk klikniety");
+        buttonT.click();
+        Runnable t1=new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("jestem w nowym watku");
             }
         };
-        button.click();
-        button.mouseOn();
-    }
-    public static void main(String[] args) {
+        t1.run();
 
+        Runnable r1 =() -> System.out.println("lambda watek");
+        r1.run();
+        //w interfejsie jedna metoda
+        // <lista parametrów> -> <ciało wyrażenia>
+
+        Button btn=new Button();
+        btn.setText("Test");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("przycisk został klikniety");
+            }
+        });
     }
 }
